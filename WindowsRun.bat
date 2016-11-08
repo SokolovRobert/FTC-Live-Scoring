a6f53be85496ffba8c82a8ac4fd677dd77170e14
Sokolov <SokolovRobert@gmail.com>
2016-11-08 00:37:00 -0500

    added to homepage

--- WindowsRun.bat
+++ WindowsRun.bat
@@ -1,3 +1,12 @@
-cd FTC-Live-Scoring
-cd src
-node index.js
+@echo OFF
+
+call start.bat
+cd %~dp0\FTC-Live-Scoring\src\
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+start "" http://localhost
+
+if %OS%==32BIT (node32.exe index.js)
+if %OS%==64BIT (node64.exe index.js)
+
+node32.exe index.js
