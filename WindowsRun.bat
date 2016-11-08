7310f9268737b2febdf2448651443f5dd9701a44
Sokolov <SokolovRobert@gmail.com>
2016-11-08 01:35:02 -0500

    updated port info to solve root-permissions issue

--- WindowsRun.bat
+++ WindowsRun.bat
@@ -4,7 +4,7 @@ call start.bat
 cd %~dp0\FTC-Live-Scoring\src\
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
-start "" http://localhost
+start "" http://localhost:8080
 
 if %OS%==32BIT (node32.exe index.js)
 if %OS%==64BIT (node64.exe index.js)
--- start.bat
+++ start.bat
@@ -2,8 +2,8 @@ set ip_address_string="IP Address"
 rem Uncomment the following line when using Windows 7 or Windows 8 / 8.1 (with removing "rem")!
 set ip_address_string="IPv4 Address"
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do (
-    echo Your IP Address is: %%f
-    echo var ip ="%%f"; > FTC-Live-Scoring/src/static/js/local.js
+    echo Your IP Address is: %%f:8080
+    echo var ip ="%%f:8080"; > FTC-Live-Scoring/src/static/js/local.js
     echo Go to the address listed above, you may need to add http:// in front.
     goto :eof
 )
